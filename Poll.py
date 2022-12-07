e26db81d8ec5399c5e2b405646d4dc2b633fd2fe
Nic <81450028+Nic2311@users.noreply.github.com>
2022-12-07 12:54:35 +0100

    Delete Poll.py

--- Poll.py
+++
@@ -1,6 +0,0 @@
-@client.command()
-async def poll(ctx,*,message):
-   emb=discord.Embed(tile="POLL",description=f"(message}")
-   msg=await ctx.channel.send(embed=emb)
-   await msg.add_reaction('👆')
-   await msg.add_reaction('👇')
